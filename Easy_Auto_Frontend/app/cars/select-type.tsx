b85717acd1e5a51b730dc635bdc1a242614946c3
Pasindumi <139098592+Pasindumi@users.noreply.github.com>
2026-04-23 15:27:42 +0530

    vehicle type icon configure

--- Easy_Auto_Frontend/app/cars/select-type.tsx
+++ Easy_Auto_Frontend/app/cars/select-type.tsx
@@ -147,7 +147,7 @@ export default function SelectVehicleTypeScreen() {
                 >
                     <View style={styles.cardImageContainer}>
                         <RNImage
-                            source={{ uri: CATEGORY_IMAGES[item.type_name] || CATEGORY_IMAGES['default'] }}
+                            source={{ uri: item.type_image || CATEGORY_IMAGES[item.type_name] || CATEGORY_IMAGES['default'] }}
                             style={styles.cardImage}
                         />
                         <LinearGradient
